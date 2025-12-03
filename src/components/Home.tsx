7e28005496b77900a9b99839062d4c8373233868
GaragesWirral <newsletterproductoffers@gmail.com>
2025-12-03 17:23:53 +0000

    feat: Add Methodology and WhyUs components to Home
    Integrates new sections to the homepage, enhancing the user journey by showcasing the agency's methodology and unique selling propositions.
    Includes updated favicon.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { Hero } from './Hero';
 import { AgencyDifference } from './AgencyDifference';
@@ -5,6 +6,8 @@ import { Services } from './Services';
 import { Testimonials } from './Testimonials';
 import { Contact } from './Contact';
 import { SeoWhyHiring } from './SeoWhyHiring';
+import { Methodology } from './Methodology';
+import { WhyUs } from './WhyUs';
 
 export const Home: React.FC = () => {
   return (
@@ -13,6 +16,8 @@ export const Home: React.FC = () => {
       <SeoWhyHiring />
       <AgencyDifference />
       <Services />
+      <Methodology />
+      <WhyUs />
       <Testimonials />
       <Contact />
     </>
